e32ee3639a91bfee3262d5acc5ee5a42b0a59078
orozhkovan <orozhkovan@eva.ua>
2023-08-16 22:17:29 +0300

    fix

---
+++ github-battle/src/redux/battle/battle.actions.js
@@ -0,0 +1,20 @@
+export const BATTLE = {
+  SET_PLAYER_NAME: "SET_PLAYER_NAME",
+  SET_PLAYER_IMAGE: "SET_PLAYER_IMAGE",
+  RESET_PLAYER: "RESET_PLAYER",
+};
+
+export const setPlayerName = (payload) => ({
+  type: BATTLE.SET_PLAYER_NAME,
+  payload: payload,
+});
+
+export const setPlayerImage = (payload) => ({
+  type: BATTLE.SET_PLAYER_IMAGE,
+  payload: payload,
+});
+
+export const resetPlayer = (payload) => ({
+  type: BATTLE.RESET_PLAYER,
+  payload: payload,
+});
